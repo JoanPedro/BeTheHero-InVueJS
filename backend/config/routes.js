1b0eec654667562533e6c30d6a608514a7a306af
Joan Pedro <jpos.senai@gmail.com>
2020-03-24 15:10:38 -0300

    Adiciona Componentes Iniciais

---
+++ backend/config/routes.js
@@ -0,0 +1,21 @@
+
+
+module.exports = app => {
+    /*
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signin)
+    app.post('/validateToken', app.api.auth.validateToken)
+    app.put('/users/:id', app.api.user.save)
+
+    app.route('/users')
+        .all(app.config.passport.authenticate())
+        .post(adminMaster(app.api.user.save))
+        .get(adminMaster(app.api.user.get))
+
+    app.route('/users/:id')
+        .all(app.config.passport.authenticate())
+        .put(adminMaster(app.api.user.save))
+        .get(adminMaster(app.api.user.getById))
+        .delete(adminMaster(app.api.user.remove))
+    */
+}
---
+++ backend/knexfile.js
@@ -0,0 +1,34 @@
+module.exports = {
+    
+    /*
+    client: 'postgresql',
+    connection: {
+        database: 'knowledge',
+        user: 'postgres',
+        password: '123456'
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        tableName: 'knex_migrations'
+    }*/
+    
+    client: 'postgresql',
+    connection: {
+        host: '...',
+        database: '...',
+        user: '...',
+        password: '...'
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+    migrations: {
+        tableName: 'knex_migrations'
+    }
+    
+
+};
