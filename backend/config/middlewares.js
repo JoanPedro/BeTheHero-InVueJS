6d0ea670bd81086b0734b869108bd410b2655231
Joan Pedro <jpos.senai@gmail.com>
2020-03-24 16:08:10 -0300

    Atualiza Pacote (Sem SQLITE)

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 module.exports = app => {
+    // Pode usar também o Express.json()
     app.use(bodyParser.json())
     app.use(cors())
 }
